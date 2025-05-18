392b068c9b5b9bdd807d381d24daf2e33ef78c11
mahelikaa <imahelika@gmail.com>
2025-05-18 21:19:31 +0530

    instructions added

--- marketplace/programs/marketplace/src/instructions/initialize.rs
+++ marketplace/programs/marketplace/src/instructions/initialize.rs
@@ -1,9 +1,52 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token_interface::{Mint, TokenInterface};
+use crate::state::Marketplace;
+
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(name: String)]
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub admin: Signer<'info>,
 
-pub fn handler(ctx: Context<Initialize>) -> Result<()> {
-    msg!("Greetings from: {:?}", ctx.program_id);
-    Ok(())
+    #[account(
+        init,
+        payer = admin,
+        seeds = [b"marketplace", name.as_bytes()],
+        bump,
+        space = Marketplace::INIT_SPACE
+    )]
+    pub marketplace: Account<'info, Marketplace>,
+    #[account(
+        seeds = [b"treasury", marketplace.key().as_ref()],
+        bump,
+    )]
+    pub treasury: SystemAccount<'info>,
+
+    #[account(
+        init,
+        payer = admin,
+        seeds = [b"rewards", marketplace.key().as_ref()],
+        bump,
+        mint::decimals = 6,
+        mint::authority = marketplace,
+    )]
+    pub rewards_mint: InterfaceAccount<'info, Mint>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Interface<'info, TokenInterface>,
+}
+
+impl<'info> Initialize<'info> {
+    pub fn init(&mut self, name: String, fee: u16, bumps: &InitializeBumps) -> Result<()> {
+    
+        self.marketplace.set_inner(Marketplace {
+            admin: self.admin.key(),
+            fee,
+            bump: bumps.marketplace,
+            treasury_bump: bumps.treasury,
+            reward_bump: bumps.rewards_mint,
+            name,
+        });
+        Ok(())
+    }
 }
